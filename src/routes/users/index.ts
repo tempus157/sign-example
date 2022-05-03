f638bb574d08c7e6247b5c83325df3f48389cf82
Park Changyong <tempus157@gmail.com>
2022-05-03 12:16:18 +0900

    Implement users router

--- src/routes/users/index.ts
+++ src/routes/users/index.ts
@@ -1,8 +1,30 @@
 import { Router } from "express";
+import { UserModel } from "../../models/user";
+
+export interface UserInfo {
+  email: string;
+  password: string;
+  name: string;
+}
+
+function isUserInfo(info: UserInfo): info is UserInfo {
+  return !!info && !!info.email && !!info.password && !!info.name;
+}
 
 const path = "/users";
 export const router = Router();
 
-router.get(path, (req, res) => {
-  res.send("Sign up");
+router.get(path, async (req, res) => {
+  const info = req.body;
+  if (!isUserInfo(info)) {
+    return res.status(400);
+  }
+
+  try {
+    await UserModel.create({ ...info });
+  } catch {
+    return res.status(409);
+  }
+
+  return res.status(201);
 });
